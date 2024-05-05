e387c0d92cd7b2f1eab441e94b0645fbe6b41d37
Anthony Ngonde <anto.ngonde@gmail.com>
2024-05-05 16:17:31 +0300

    App completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,3 +2,27 @@ import React, { useState } from 'react';
 import TransactionTable from './TransactionTable';
 import TransactionForm from './TransactionForm';
 import SearchBar from './SearchBar';
+
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleAddTransaction = (newTransaction) => {
+    setTransactions([...transactions, newTransaction]);
+  };
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>Bank of Flatiron</h1>
+      <TransactionForm onAddTransaction={handleAddTransaction} />
+      <SearchBar onSearch={setSearchTerm} />
+      <TransactionTable transactions={filteredTransactions} />
+    </div>
+  );
+};
+
+export default App;
